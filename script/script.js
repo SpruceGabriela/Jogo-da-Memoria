bef058fa170afecebacc54d5c73f59ae357a78a9
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2019-07-22 17:38:22 -0300

    estilizar layout

---
+++ script/script.js
@@ -0,0 +1,36 @@
+function embaralhar(lista){
+    let temporario;
+    let aleatorio;
+
+    for(let i = lista.length - 1; i !== 0; i--) {
+
+        aleatorio = Math.floor(Math.random() * i);
+
+        temporario = lista[i];
+        lista[i] = lista[aleatorio];
+        lista[aleatorio] = temporario;
+
+    }
+    return lista;
+}
+
+function mostrar(cartas){
+    cartas.style.backgroundImage = `url('images/${concatImagens[Number(cartas.id)]}')`;
+    cartas.onclick = null;
+}
+
+
+let cartas = document.querySelectorAll('.cartas');
+
+let primeiraCarta;
+let segundaCarta;
+
+let imagens = ['rafiki.png', 'scar.png', 'shenzi.png', 'simba.png', 'timao.png', 'zazu.png'];
+
+let concatImagens = imagens.concat(imagens);
+
+concatImagens = embaralhar(concatImagens);
+
+for(carta of cartas){
+    mostrar(carta);
+}
